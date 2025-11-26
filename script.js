7a42db67130562f3b8d7bd0c67197581e248dda6
Hieu Trung Vu (Trung Vu) <hvu@illinois.edu>
2025-11-26 12:55:00 +0800

    website

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Example: Highlight header on click
-    const header = document.querySelector('.header');
-    if (header) {
-        header.addEventListener('click', function() {
-            header.style.color = '#7fdfff';
-            setTimeout(() => header.style.color = '', 700);
-        });
-    }
+    // ...existing code...
 });
